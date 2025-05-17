56f2f6a47e05c0e4c1183dc181fccddd410f087f
BefkaduB <befkadu.beletee@gmail.com>
2025-05-17 16:00:46 +0300

    Collapsable Group menu

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,9 +1,172 @@
-import React from 'react'
+import React from "react";
+import {
+  Home,
+  Inbox,
+  Calendar,
+  Search,
+  Settings,
+  User2,
+  ChevronUp,
+  Plus,
+  Projector,
+  ChevronDown,
+} from "lucide-react";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSubButton,
+  SidebarSeparator,
+} from "@/components/ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
 
 export default function AppSidebar() {
   return (
-    <div>
-      <h1>SID BAR</h1>
-    </div>
-  )
+    <Sidebar collapsible="icon" side="left">
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuButton asChild>
+            <SidebarMenuItem>
+              <Link href="/">
+                <Image src="/next.svg" alt="Logo" width={50} height={50} />
+              </Link>
+              <span>All Coder</span>
+            </SidebarMenuItem>
+          </SidebarMenuButton>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarSeparator className="px-2" />
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel> ABCD</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <Link href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+
+        <SidebarGroup>
+          <SidebarGroupLabel>Projects</SidebarGroupLabel>
+          <SidebarGroupAction>
+            <Plus /> <span className="sr-only">Add Project</span>
+          </SidebarGroupAction>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuButton asChild>
+                <Link href={"/"}>
+                  <Projector /> See All Project
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <Collapsible defaultOpen className="group/collapsible">
+          <SidebarGroup>
+            <SidebarGroupLabel asChild>
+              <CollapsibleTrigger>
+                Collapsable Group
+                <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+              </CollapsibleTrigger>
+            </SidebarGroupLabel>
+
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuButton asChild>
+                    <Link href={"/"}>
+                      <Projector /> See All Project
+                    </Link>
+                  </SidebarMenuButton>
+                  <SidebarMenuButton asChild>
+                    <Link href={"/"}>
+                      <Projector /> See All Project
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
+      </SidebarContent>
+
+      <SidebarFooter>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuSubButton>
+                  <User2 /> Befkadu B. <ChevronUp className="ml-auto" />
+                </SidebarMenuSubButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem> Account </DropdownMenuItem>
+                <DropdownMenuItem> Account </DropdownMenuItem>
+                <DropdownMenuItem> Account </DropdownMenuItem>
+                <DropdownMenuItem> Account </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarFooter>
+    </Sidebar>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
-"use client"
-import { LogOut, Moon,  Settings, Sun, User } from "lucide-react";
+"use client";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
 import Link from "next/link";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
@@ -12,42 +12,47 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
-
+import { SidebarTrigger, useSidebar } from "./ui/sidebar";
 
 const Navbar = () => {
-const {setTheme} = useTheme()
+  const { setTheme } = useTheme();
+  const { toggleSidebar } = useSidebar();
   return (
     <nav className="p-4 flex items-center justify-between">
       {/* LEFT */}
-      collapsButton
+      {/* <SidebarTrigger/> */}
+      <Button variant={"outline"} onClick={toggleSidebar}>
+        {" "}
+        Custom Button{" "}
+      </Button>
       {/* RIGHT */}
       <div className="flex items-center gap-4">
         <Link href="/"> Dashboard</Link>
         {/* THEME MENU */}
         <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-       
-{/* USER MENU */}
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        {/* USER MENU */}
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar>
@@ -58,10 +63,16 @@ const {setTheme} = useTheme()
           <DropdownMenuContent sideOffset={10}>
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
-            <DropdownMenuItem> <User className="h-[1.2rem] w-[1.2rem] mr-2"/> Profile</DropdownMenuItem>
-            <DropdownMenuItem><Settings className="h-[1.2rem] w-[1.2rem] mr-2"/> Setting </DropdownMenuItem>
-            <DropdownMenuItem variant={"destructive"}><LogOut className="h-[1.2rem] w-[1.2rem] mr-2"/> Logout</DropdownMenuItem>
-           
+            <DropdownMenuItem>
+              {" "}
+              <User className="h-[1.2rem] w-[1.2rem] mr-2" /> Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="h-[1.2rem] w-[1.2rem] mr-2" /> Setting{" "}
+            </DropdownMenuItem>
+            <DropdownMenuItem variant={"destructive"}>
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" /> Logout
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
